5698eb05c7aac27249c6e5432dff5cc5d5f31533
NicolasJaved <nicolas.javed512@gmail.com>
2018-10-07 10:59:21 +0200

    refactored GameViewController to reduce functions size and remained functions for clarity

--- NihonGame/MVC/VC/GameViewController.swift
+++ NihonGame/MVC/VC/GameViewController.swift
@@ -15,11 +15,7 @@ class GameViewController: UIViewController {
     var question : Question? = nil {
         didSet{
             if let question = question {
-                let questionMode = SQModeView(frame: self.view.frame)
-                questionMode.question = question
-                NotificationCenter.default.addObserver(self, selector: #selector(userhasAnswered), name: notificationName, object: nil)
-                view.addSubview(questionMode)
-                self.questionMode = questionMode
+                setQuestionMode(question: question)
             }
         }
     }
@@ -39,10 +35,10 @@ class GameViewController: UIViewController {
     }
 
     override func viewDidAppear(_ animated: Bool) {
-        initLevel()
+        initQuestion()
     }
 
-    func initLevel() {
+    func initQuestion() {
         guard let level = level, let currentModeID = tabBarController?.selectedIndex else {
             return
         }
@@ -54,26 +50,42 @@ class GameViewController: UIViewController {
         }
     }
 
+    func setQuestionMode(question: Question) {
+        let questionMode = SQModeView(frame: self.view.frame)
+        questionMode.question = question
+        NotificationCenter.default.addObserver(self, selector: #selector(userhasAnswered), name: notificationName, object: nil)
+        view.addSubview(questionMode)
+        self.questionMode = questionMode
+    }
+
     @objc func userhasAnswered() {
         if let questionMode = questionMode{
             if questionMode.answered.hasAnswered {
-                let popUpImage = PopUpImageView(parentframe: self.view.frame, size: CGSize(width: self.view.frame.width, height: 100))
-                if questionMode.answered.hasCorrectlyAnswered {
-                    popUpImage.image = UIImage(named: "Correct")!
-                }else {
-                    popUpImage.image = UIImage(named: "NotCorrect")!
-                }
+                let popUpImageView = setAnswerStatutView(isCorrect: questionMode.answered.hasCorrectlyAnswered)
 
                 if let questionModeasView = questionMode as? UIView {
                     questionModeasView.removeFromSuperview()
-                    self.view.addSubview(popUpImage)
+                    self.view.addSubview(popUpImageView)
                 }
                 DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(500)) {
-                    popUpImage.removeFromSuperview()
-                    self.initLevel()
+                    popUpImageView.removeFromSuperview()
+                    self.initQuestion()
                 }
             }
         }
     }
 
+    private func setAnswerStatutView(isCorrect: Bool) -> PopUpImageView{
+        let popUpImageView = PopUpImageView(parentframe: self.view.frame, size: CGSize(width: self.view.frame.width, height: 100))
+        var image = UIImage(named: "NotCorrect")
+        if isCorrect {
+            image = UIImage(named: "Correct")
+        }
+
+        if let image = image {
+            popUpImageView.image = image
+        }
+
+        return popUpImageView
+    }
 }
