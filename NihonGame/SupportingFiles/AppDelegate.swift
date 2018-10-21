7d597012b51e84077257d3929b4673d8d37abc94
NicolasJaved <nicolas.javed512@gmail.com>
2018-10-21 12:06:49 +0200

    merged

--- NihonGame/SupportingFiles/AppDelegate.swift
+++ NihonGame/SupportingFiles/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // ----- create windows and VC
+        // ----- create windows and VC 
         let mainCoordinator = MainCoordinator(appDelegate: self)
         mainCoordinator.start()
         return true
